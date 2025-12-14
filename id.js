e682e1b5caa7ebe0b0f040f926532d3869490ffe
Xnodes Development <xnodesdev@gmail.com>
2025-12-14 17:30:01 +0530

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,9 @@
-function giftedid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 2; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
-  }
-  return result;
+// Generates a random alphanumeric string
+export function giftedid(length = 4) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
 }
-module.exports = {giftedid};
